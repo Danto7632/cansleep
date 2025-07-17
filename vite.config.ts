15a780e8aee7c539427e38aa181ad355fd5c0e0e
Danto <rlarladlstjd@gmail.com>
2025-07-17 15:53:36 +0900

    domain 적용

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cansleep/', // GitHub Pages의 repository 이름으로 설정
+  base: '/', // 커스텀 도메인 사용을 위해 루트로 설정
 });
